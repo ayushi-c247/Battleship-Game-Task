c88d4213f1c284de9f64dab46c7e3a71243db519
ayushiC247 <ayushi.pc247@chapter247.com>
2021-04-15 10:19:11 +0530

    making ship vertically and horizontally  and its all possible direction like up, down,left,right

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
-let userGridSize = prompt('What size would you like your grid to be?');
-let enemyGridSize = myGridSize;
-let userGrid = createGrid(userGridSize);
-let enemyGrid = createGrid(enemyGridSize);
-let userShips = 3;
-let enemyShips = 3;
+//initialize variables
+let flag = 0;
+let userBoardSize = prompt('What size would you like your grid to be?');
+let enemyBoardSize = userBoardSize;
+let userBoard = createGrid(userBoardSize);
+let enemyBoard = createGrid(enemyBoardSize);
+let userAttacks = 3;
+let enemyAttacks = 3;
+let enemyLocations = {};
+printGrid(enemyBoard, true);
+printGrid(userBoard);
+gameSetup();
 
-printGrid(enemyGrid, true);
-printGrid(userGrid);
 
+//game setup
+function gameSetup() {
+    for (let totalShip = 1; totalShip <= 1; totalShip++) {
+        let x = prompt('Enter the x coordinate for your ship placement' + totalShip);
+        let y = prompt('Enter the y coordinate for your ship placement' + totalShip);
+        if (userBoardSize >= x && userBoardSize >= y) {
+            let shipView = prompt('Enter "yes" if, you want ship vertically otherwise enter "no"');
+            if (shipView.toLowerCase() === 'yes' && shipView !== " " && shipView.toLowerCase() !== "no") {
+                placeCharacterVertical(x, y, 'O', userBoard);
+            } else {
+                placeCharacter(x, y, 'O', userBoard);
+            }
+            enemy('O', enemyBoard, enemyBoardSize);
+            //enemy((x, y, 'O', enemyBoard);
+            drawBreak();
+            printGrid(enemyBoard, true);
+            printGrid(userBoard);
+        } else {
+            alert('Your coordinate value is wrong please enter correct coordinate');
+            // confirm('You want to continue! click ok');
+            gameSetup();
+        }
+    }
+}
+
+
+//game loop
+while (enemyAttacks > 0 && userAttacks > 0) {
+    let x = prompt('Enter the x coordinate for your attack');
+    let y = prompt('Enter the y coordinate for your attack');
+    if (attack(x, y, enemyBoard)) {
+        enemyAttacks--;
+    }
+    x = getRandomInt(userGridSize);
+    y = getRandomInt(userGridSize);
+    if (enemyAttacks > 0 && attack(x, y, userBoard)) {
+        userAttacks--;
+    }
+
+    drawBreak();
+    printGrid(enemyBoard, true);
+    printGrid(userBoard);
+}
+
+
+//lose Win Result 
+if (userAttacks < enemyAttacks) {
+    console.log('Lose it all!');
+} else {
+    console.log('Victory!!!');
+}
+
+
+//CreateGrid
 function createGrid(size) {
     let grid = [];
     for (let i = 0; i < size; i++) {
@@ -19,22 +77,25 @@ function createGrid(size) {
     return grid;
 }
 
+
+//printGrid
 function printGrid(grid, isEnemy = false) {
     const headers = createHeaders(grid.length);
     console.log(headers);
     for (let i = 0; i < grid.length; i++) {
-        let rowStr = i + ' ';
+        let rows = i + ' ';
         for (let cell of grid[i]) {
             if (isEnemy && cell == 'O') {
-                rowStr += '- ';
+                rows += '- ';
             } else {
-                rowStr += cell + ' ';
+                rows += cell + ' ';
             }
         }
-        console.log(rowStr);
+        console.log(rows);
     }
 }
 
+//
 function createHeaders(size) {
     let result = '  ';
     for (let i = 0; i < size; i++) {
@@ -43,3 +104,76 @@ function createHeaders(size) {
     return result;
 }
 
+
+//horizontally ship view 
+function placeCharacter(x, y, c, grid) {
+    grid[parseInt(y)][parseInt(x)] = c;
+    //left
+    let leftRightShipPosition = prompt('"Enter "left" if you want left side and Enter "right" if you want right side  ');
+    if (leftRightShipPosition.toLowerCase() === "left" && leftRightShipPosition.toLowerCase() !== "right" && leftRightShipPosition.toLowerCase() !== "") {
+        grid[parseInt(y)][parseInt(x) - 1] = c;
+        grid[parseInt(y)][parseInt(x) - 2] = c;
+    }
+    //right
+    //if (leftRightShipPosition.toLowerCase() === "right" && leftRightShipPosition.toLowerCase() == "")
+    else {
+        grid[parseInt(y)][parseInt(x) + 1] = c;
+        grid[parseInt(y)][parseInt(x) + 2] = c;
+    }
+}
+
+//vertically ship view 
+function placeCharacterVertical(x, y, c, grid) {
+    grid[parseInt(x)][parseInt(y)] = c;
+
+    //Up
+    let upDownShipPosition = prompt('"Enter "Up" if you want up side and Enter "Down" if you want Down side  ');
+    if (upDownShipPosition.toLowerCase() === "up" && upDownShipPosition.toLowerCase() !== "down" && upDownShipPosition.toLowerCase() !== "") {
+        grid[parseInt(x) - 1][parseInt(y)] = c;
+        grid[parseInt(x) - 2][parseInt(y)] = c;
+    } else {
+        //down
+        grid[parseInt(x) + 1][parseInt(y)] = c;
+        grid[parseInt(x) + 2][parseInt(y)] = c;
+    }
+}
+
+
+//enemy
+function enemy(c, grid, max) {
+    c = 'e';
+    let didPlace = false;
+    while (!didPlace) {
+        let x = getRandomInt(max);
+        let y = getRandomInt(max);
+        if (!enemyLocations[`${x}-${y}`]) {
+            placeCharacter(x, y, c, grid);
+            didPlace = true;
+            enemyLocations[`${x}-${y}`] = true;
+        }
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+//attacks
+function attack(x, y, grid) {
+    if (grid[y][x] == 'O') {
+        grid[y][x] = '!';
+        return true;
+    } else if (grid[y][x] == '-') {
+        grid[y][x] = 'x';
+        return false;
+    } else {
+        return false;
+    }
+}
+
+
+//result
+function drawBreak() {
+    console.log('****************************************Game Result***********************************************');
+}
